a73e388a7d849498d0aef9c64b39e21e7c07ccfd
liujianhua <theliujianhua@qq.com>
2014-11-22 20:49:13 +0800

    add example: gallery

---
+++ test/example.js
@@ -0,0 +1,17 @@
+var mod, age;
+var beatles;
+var years;
+
+mod = "happy";
+age = 33;
+
+beatles = ["John", "Paul", "George", "Ringo"];
+years = [2012, 2013, 2014, 2015];
+//alert(mod);
+//alert(age);
+
+Object.print = function(o) {console.log(o);}
+
+var o = new Object();
+o.print(o)
+
